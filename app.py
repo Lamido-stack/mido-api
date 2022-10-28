cc4e7ae9dc21a2bd0f7a7869b305ebdaee60056c
Lamido <uklamido@gmail.com>
2022-10-28 21:27:09 +0100

    yolo

--- app.py
+++ app.py
@@ -7,9 +7,14 @@ app = flask.Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def home():
+    return 'Home'
+
+
+@app.route("/mido")
+def test():
     data_set = {'slackUsername': 'Lamido', 'backend': True, 'age': 22, 'bio': 'Im a tech enthusiast on a journey of self realisation'}
     json_dump = json.dumps(data_set)
     return json_dump
 
-
-home()
+if __name__=="__main__":
+    app.run() 
