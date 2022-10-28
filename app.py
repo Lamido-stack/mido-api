beeb576bd90abc377b3dbb5b6a07495de79a821d
Lamido <uklamido@gmail.com>
2022-10-28 22:25:47 +0100

    lippy

--- app.py
+++ app.py
@@ -7,6 +7,5 @@ app = flask.Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def home():
-    data_set = {'slackUsername': 'Lamido', 'backend': True, 'age': 22, 'bio': 'Im a tech enthusiast on a journey of self realisation'}
-    json_dump = json.dumps(data_set)
-    return json_dump
+    data = json.load(open('data.json'))
+    return data
