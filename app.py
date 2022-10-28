7b713844ec95edd422cbfe0e98cf6db6b8133507
Lamido <uklamido@gmail.com>
2022-10-28 20:33:25 +0100

    third commit

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import flask
 from flask import request
+import json
 
 app = flask.Flask(__name__)
 
 
 @app.route('/', methods=['GET'])
 def home():
-    data_set = {"slackUsername": "Lamido", "backend": True, "age": 22, "bio": "Im a tech enthusiast on a journey of self realisation" }
-    arg = request.args['arg1']
-    return data_set
+    data_set = {'slackUsername': 'Lamido', 'backend': True, 'age': 22, 'bio': 'Im a tech enthusiast on a journey of self realisation'}
+    json_dump = json.dumps(data_set)
+    return json_dump
